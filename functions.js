2e9de9115cf52baf204f9474099ede3f5177b32c
PabloGutierrez1 <genesislol660@gmail.com>
2024-03-14 17:34:39 -0300

    Agregado funcionalidades js y boton de dark mode, cambios en el estilo de la navbar

---
+++ functions.js
@@ -0,0 +1,22 @@
+let parrafo = document.getElementById("p");
+let btn = document.getElementById("dark-mode");
+
+const maquinadeescribir = (text = "", tiempo = 200, etiqueta = "") => {
+    let array_caracteres = text.split("");
+    let cont = 0;
+    etiqueta.innerHTML = "";
+    let escribir = setInterval(function(){
+        etiqueta.innerHTML += array_caracteres[cont];
+        cont++;
+        if(cont == array_caracteres.length){
+            clearInterval(escribir);
+        }
+    }, tiempo)
+}
+
+btn.addEventListener("click", () => {
+    btn.classList.toggle("cambios");
+})
+
+maquinadeescribir("Hola me llamo Pablo Gutierrez, tengo 17 años y soy desarrollador full-stack Trainee, este es mi portafolio personal, espero que sea agradable a la vista.", 50, parrafo);
+
